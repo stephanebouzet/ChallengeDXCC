92a80d6fa65e277117699441c76d57362d767587
stephanebouzet <stephane.bouzet@gmail.com>
2026-09-22 19:22:52 +0200

    Exclure build/

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ChallengeDXCC_FILE D:/Programmation/ChallengeDXCC/build/ChallengeDXCC.exe)
-set(__QT_DEPLOY_TARGET_ChallengeDXCC_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ChallengeDXCC_RUNTIME_DLLS C:/Qt/6.8.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6WebSockets.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Network.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
